444055e9336ccb846891b22f200ceb48734cf5ca
hassasraf <hassasraf@gmail.com>
2021-06-08 20:29:31 +0600

    Initial git commit

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ $(document).ready(function(){
 	$('#banner-area .owl-carousel').owlCarousel({
 		items:1,
 		loop:true,
-		autoplay:true
+		autoplay:true,
+		dots:true,
+		nav:false
+	});
+
+	// top sale owl-carousel
+	$('#top-sale .owl-carousel').owlCarousel({
+		loop:true,
+		nav:true,
+		dots:false,
+		autoplay:true,
+		margin:10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:false
+			},
+			1000:{
+				items:5,
+				nav:true,
+				loop:false
+			}
+		}
 	})
     
 })
